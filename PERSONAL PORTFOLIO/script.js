b268baceb6448a591b3dea6ee1409c7a720312ee
COMRADE-TITHY <120745940+COMRADE-TITHY@users.noreply.github.com>
2023-03-24 18:28:52 +0530

    Add files via upload

---
+++ PERSONAL PORTFOLIO/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
+
+  //Dark Mode
+  let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
